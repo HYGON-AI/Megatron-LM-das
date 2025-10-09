c6d533d9cf4122580c759cfc18772e9ab025e683
dongcl <791582849@qq.com>
2025-10-09 17:08:52 +0800

    remove duplicate code

--- dcu_megatron/adaptor/megatron_adaptor.py
+++ dcu_megatron/adaptor/megatron_adaptor.py
@@ -229,23 +229,6 @@ class CoreAdaptation(MegatronAdaptationABC):
                                     moe_layer_init_wrapper)
         MegatronAdaptation.register('megatron.core.transformer.moe.moe_layer.MoELayer.forward',
                                     moe_layer_forward_wrapper)
-        # fused gelu and mul
-        MegatronAdaptation.register('megatron.core.transformer.moe.experts.TEGroupedMLP.forward',
-                                    TEGroupedMLP.forward)
-        # cpu offload.
-        MegatronAdaptation.register('megatron.core.transformer.attention.Attention.__init__',
-                                    attention_init_wrapper,
-                                    apply_wrapper=True)
-        MegatronAdaptation.register('megatron.core.transformer.moe.experts.TEGroupedMLP.__init__',
-                                    te_grouped_mlp_init_wrapper,
-                                    apply_wrapper=True)
-        MegatronAdaptation.register('megatron.core.transformer.transformer_layer.TransformerLayer.__init__',
-                                    transformer_layer_init_wrapper,
-                                    apply_wrapper=True)
-        MegatronAdaptation.register('megatron.core.transformer.mlp.MLP.__init__',
-                                    mlp_init_wrapper,
-                                    apply_wrapper=True)
-
         # (1) activation offload, (2) use_qk_norm
         MegatronAdaptation.register('megatron.core.transformer.attention.SelfAttention.get_query_key_value_tensors',
                                     SelfAttention.get_query_key_value_tensors)
